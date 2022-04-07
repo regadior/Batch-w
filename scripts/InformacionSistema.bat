1d6be740010dda31aa7cf6d72cfa6f109c6e72cd
regadior <regadior@gmail.com>
2022-04-07 13:08:16 +0200

    ejer info sistema

--- scripts/InformacionSistema.bat
+++ scripts/InformacionSistema.bat
@@ -2,14 +2,15 @@
 :: 1. Presentar data e hora
 :: 2. Crear info.txt
 ::    -sistema operativo
-::    -versiÃ³n do S.O.
+::    -versi¢n do S.O.
 ::    -Etiqueta volume de disco
 ::    -usuario
 ::9.sair
 
-::Introduce una opciÃ³n
+::Introduce una opci¢n
 
 @ECHO OFF
+:menu
 CLS
 ECHO ***************************
 ECHO *** INFORMACION SISTEMA ***
@@ -22,22 +23,45 @@ ECHO    -Version del S.O.
 ECHO    -Etiqueta volume disco
 ECHO    -usuario
 ECHO 9. Sair
-ECHO.
-ECHO Introduzca una opcion
-pause > nul
-CLS
-SET opcion=0
 
+:opcion
+    /set /p opc=Intrduce una opci¢n(Intro para acabar) :
+    if "%opc%" equ "1" goto fechaHora
+    if "%opc%" equ "1" crearInfo
+    if "%opc%" equ "1" salir
+    echo.
+    echo Opci¢n no v lida 
+goto opcion
 
-OPCIONES:
-IF %opcion%==1 (
-    ECHO date /t
-    ECHO time /t
-)
-IF %opcion%==2 (
-    COPY CON info.txt
-)
-IF %opcion%==9 (
+:fechaHora
+    cls
+    echo.
+    echo %date%
+    echo %time%
+    echo.
+    pause
+goto menu
 
+:crearInfo
+    cls
+    echo.
+    echo %os% > info2.txt
+    ver >> info2.txt
+    vol >> info2.txt
+    echo %username% >> info2.txt
+    echo.
+    echo Se gener¢ info2.txt con la siguiente informaci¢n
+    ::para q el usuario vea c¢mo qued¢ el archivo:
+    type info2.txt
+    pause
+goto menu
 
+:salir
+echo.
+echo Fin del programa
+echo.
+::liberamos las variables al acabar el procedimiento poniendo abajo el = sin nada.
+set opc=
+pause
+exit
 
